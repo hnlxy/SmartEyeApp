e04a436eacbd2c034020c765aadd6c9c9b334c79
cck <2385966053@qq.com>
2023-11-25 23:56:09 +0800

    添加注册

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,59 +1,50 @@
 <template>
-    <view class="uni-container container">
-        <view class="uni-title subtitle">
-            <text class="uni-subtitle-text">登录</text>
-        </view>
-        <!-- <form action="" method="post"> -->
-        <view class="uni-form-item">
-            <input class="uni-input" focus placeholder="请输入邮箱或手机号码" />
-        </view>
-        <view class="uni-form-item">
-            <input class="uni-input" focus placeholder="请输入密码" />
-        </view>
-        <view class="uni-form-item">
-            <switch checked="true" @change="" style="transform:scale(0.7)" />
-            <text class="">是否保存密码</text>
-        </view>
-        <view class="uni-form-item">
-            <button class="uni-button" @click="goto('/pages/tabbar/tabbar-1/tabbar-1')">登录</button>
-        </view>
-        <!-- </form> -->
-    </view>
+	<view class="uni-container container">
+		<view class="uni-title subtitle">
+			<text class="uni-subtitle-text">登录</text>
+		</view>
+		<!-- <form action="" method="post"> -->
+		<view class="uni-form-item">
+			<input class="uni-input" focus placeholder="请输入邮箱或手机号码" />
+		</view>
+		<view class="uni-form-item">
+			<input class="uni-input" focus placeholder="请输入密码" />
+		</view>
+		<view class="uni-form-item">
+			<switch checked="true" @change="" style="transform:scale(0.7)" />
+			<text class="">是否保存密码</text>
+		</view>
+		<view class="uni-form-item">
+			<button class="uni-button" @click="goto('/pages/tabbar/tabbar-1/tabbar-1')">登录</button>
+		</view>
+		<!-- </form> -->
+	</view>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-
-            }
-        },
-        methods: {
-            goto(url) {
-                uni.switchTab({
-                    url: url
-                })
-            }
-        }
-    }
+<script setup>
+	const goto = (url) => {
+		uni.switchTab({
+			url: url
+		});
+	}
 </script>
 
 <style>
-    .container {
-        padding: 50rpx;
-    }
+	.container {
+		padding: 50rpx;
+	}
 
-    .uni-form-item {
-        display: flex;
-        justify-content: center;
-    }
+	.uni-form-item {
+		display: flex;
+		justify-content: center;
+	}
 
-    .uni-subtitle-text {
-        color: black;
-        font-size: 50rpx;
-    }
+	.uni-subtitle-text {
+		color: black;
+		font-size: 50rpx;
+	}
 
-    .subtitle {
-        margin-top: 100rpx;
-    }
+	.subtitle {
+		margin-top: 100rpx;
+	}
 </style>
--- pages/user/register.vue
+++ pages/user/register.vue
@@ -1,22 +1,92 @@
 <template>
-    <view>
-        
-    </view>
+	<view class="container">
+		<view style="margin-top: 30%; text-align: center; font-size: 25px;">注册</view>
+		<view class="form">
+			<view class="form-item">
+				<label for="telephone">账号</label><input class="input" id="telephone" placeholder="请输入手机号"
+					@input="updateField('telephone', $event.detail.value)" />
+			</view>
+			<view class="form-item">
+				<label for="password">密码</label><input password class="input" id="password" placeholder="请输入密码"
+					@input="updateField('password', $event.detail.value)" />
+			</view>
+			<view class="form-item">
+				<label for="password2">重复密码</label><input password class="input" id="password2" placeholder="请再次输入密码"
+					@input="updateField('password2', $event.detail.value)" />
+			</view>
+			<button class="button" @click="register">提交注册</button>
+			<view class="login-link" @click="goToLogin">已有账号？返回登录</view>
+		</view>
+	</view>
 </template>
+<script setup>
+	import {
+		reactive
+	} from 'vue';
 
-<script>
-    export default {
-        data() {
-            return {
-                
-            }
-        },
-        methods: {
-            
-        }
-    }
+	const state = reactive({
+		telephone: '',
+		password: '',
+		password2: ''
+	});
+
+	const updateField = (field, value) => {
+		state[field] = value;
+	};
+
+	const register = () => {
+		// 提交表单逻辑
+		console.log(state);
+	};
+	const goToLogin = () => {
+		uni.navigateTo({
+			url: '/pages/user/login'
+		});
+	};
 </script>
 
-<style>
 
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+		padding: 20px;
+		background-color: #E0F0E9;
+	}
+
+	.title {
+		margin-top: 15%;
+		font-size: 24px;
+		color: #333;
+	}
+
+	.form {
+		width: 100%;
+	}
+
+	.form-item {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 20px;
+	}
+
+	.label {
+		margin-bottom: 5px;
+	}
+
+	.input {
+		border: 1px solid #ddd;
+		padding: 10px;
+		border-radius: 4px;
+	}
+
+	.button {
+		background-color: #007aff;
+		color: white;
+		padding: 10px;
+		border-radius: 10px;
+		line-height: 25px;
+	}
 </style>

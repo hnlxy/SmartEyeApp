f1d8ec23f663aeee0f5dfb30ba9ef2e381e6229c
pty <1773679235@qq.com>
2023-12-08 17:43:42 +0800

    增加菜单页面

---
+++ pages/tabbar-3-detial/tabbar-3-see/tabbar-3-see.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <video id="video" width="750" controls autoplay></video>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.setupVideoStream();
+  },
+  methods: {
+    setupVideoStream() {
+      // 假设你的WebSocket流在9999端口
+      const wsUrl = 'ws://localhost:9999';
+      const videoElement = document.getElementById('video');
+      // 这里需要一个适当的方法来处理WebSocket流
+      // 可以使用像'jsmpeg'这样的库来解码视频
+    }
+  }
+};
+</script>
+
+<style>
+.video {
+  width: 100%;
+  height: auto;
+}
+</style>
+

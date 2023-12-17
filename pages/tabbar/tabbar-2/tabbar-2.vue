4d731f1ef1a1b79901c9fdd215b3630631c50dd4
cck <2385966053@qq.com>
2023-12-17 18:22:32 +0800

    添加统计页面

--- pages/tabbar/tabbar-2/tabbar-2.vue
+++ pages/tabbar/tabbar-2/tabbar-2.vue
@@ -1,96 +1,225 @@
 <template>
-	<view class="notifications-container">
-		<view class="notification" v-for="item in notifications" :key="item.id" @click="goToReplay(item)">
-			<view class="avatar-container">
-				<img :src="item.avatar" class="avatar" alt="avatar" />
-			</view>
-			<view class="notification-content">
-				<view class="notification-message">{{ item.message }}</view>
-				<view class="notification-time">{{ item.time }}</view>
-			</view>
-		</view>
-	</view>
+	<div class="app">
+		<!-- 统计区域 -->
+		<div class="stats">
+			<!-- 动态显示当前选中的 Tab 统计信息 -->
+			<div class="stat-item">
+				<div class="stat-key">本周总次数</div>
+				<div class="stat-value">{{ currentStats.weekly }}</div>
+			</div>
+			<div class="stat-item">
+				<div class="stat-key">本月总次数</div>
+				<div class="stat-value">{{ currentStats.monthly }}</div>
+			</div>
+		</div>
+
+		<!-- Tab 切换区域 -->
+		<div class="tabs">
+			<div class="tab-item" v-for="(tab, index) in tabs" :key="index"
+				:class="{ active: currentTab === tabs[index].value }" @click="switchTab(index)">
+				{{ tab.label }}
+			</div>
+		</div>
+
+		<!-- 当前 Tab 内容区域 -->
+		<div class="tab-content">
+			<div class="event-item" v-for="event in currentTabEvents" :key="event.id">
+				<img :src="event.avatar" class="avatar" />
+				<div class="event-info">
+					<div class="event-name">{{ event.name }}</div>
+					<div class="event-time">{{ event.time }}</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
 	import {
-		ref
+		ref,
+		reactive,
+		computed
 	} from 'vue';
-	// 按时间排列显示
-	const notifications = ref([{
-			id: 1,
-			message: '张三 吃饭',
-			time: '2023-12-02 10:30',
-			avatar: '../../../static/img/face/face2.png'
+	const tabEvents = {
+		tab1: [{
+				id: 1,
+				name: '张三',
+				time: '2023-12-02 10:30',
+				avatar: '../../../static/img/face/face2.png'
+			},
+			{
+				id: 2,
+				name: '李四',
+				time: '2023-12-17 16:30',
+				avatar: '../../../static/img/face/face3.png'
+			},
+		],
+		tab2: [{
+				id: 1,
+				name: '王五',
+				time: '2023-12-02 10:30',
+				avatar: '../../../static/img/face/face4.png'
+			},
+			{
+				id: 2,
+				name: '赵六',
+				time: '2023-12-17 16:30',
+				avatar: '../../../static/img/face/face5.png'
+			},
+		],
+		tab3: [{
+				id: 1,
+				name: '孙七',
+				time: '2023-12-02 10:30',
+				avatar: '../../../static/img/face/face6.png'
+			},
+			{
+				id: 2,
+				name: '王八',
+				time: '2023-12-17 16:30',
+				avatar: '../../../static/img/face/face7.png'
+			},
+		]
+	};
+
+	const tabStats = {
+		tab1: {
+			weekly: 10,
+			monthly: 40
+		},
+		tab2: {
+			weekly: 5,
+			monthly: 20
+		},
+		tab3: {
+			weekly: 20,
+			monthly: 80
+		},
+	};
+
+	const tabs = reactive([{
+			label: '翻身',
+			value: 'tab1'
 		},
 		{
-			id: 2,
-			message: '李四 喝水',
-			time: '2023-12-02 10:30',
-			avatar: '../../../static/img/face/face3.png'
+			label: '喝水',
+			value: 'tab2'
 		},
 		{
-			id: 3,
-			message: '李四 翻身',
-			time: '2023-12-02 10:30',
-			avatar: '../../../static/img/face/face3.png'
-		},
-		{
-			id: 4,
-			message: '王五 喝水',
-			time: '2023-12-02 10:30',
-			avatar: '../../../static/img/face/face4.png'
+			label: '穿衣',
+			value: 'tab3'
 		},
 	]);
-	import {
-		useRouter
-	} from 'vue-router';
-	const router = useRouter();
-	const goToReplay = (item) => {
-		router.push('/pages/tabbar/tabbar-1/video_replay');
-	};
+	const currentTabEvents = computed(() => {
+		return tabEvents[currentTab.value] || [];
+	});
+	const currentTab = ref(tabs[0].value);
+
+	const currentStats = computed(() => {
+		return tabStats[currentTab.value];
+	});
+
+	function switchTab(index) {
+		currentTab.value = tabs[index].value;
+	}
 </script>
 
-
 <style scoped>
-	.notifications-container {
-		padding: 16px;
-		background-color: #EBF5F5;
+	.app {
+		font-family: 'Arial', sans-serif;
+		max-width: 600px;
+		margin: auto;
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+		border-radius: 8px;
+		overflow: hidden;
 		height: 100%;
-		overflow-y: auto;
 	}
 
-	.notification {
-		background-color: #fff;
-		border-radius: 10px;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	.stats {
+		display: flex;
+		justify-content: space-around;
+		background-color: #ffffff;
+		padding: 16px;
+		border-bottom: 1px solid #eee;
+	}
+
+	.stat-item {
+		text-align: center;
+	}
+
+	.stat-key {
+		font-size: 14px;
+		color: red;
+		margin-bottom: 8px;
+	}
+
+	.stat-value {
+		font-size: 20px;
+		color: #333333;
+		font-weight: bold;
+	}
+
+	.tabs {
+		display: flex;
+		background-color: #f7f7f7;
+		overflow: auto;
+		/* 允许水平滚动 */
+	}
+
+	.tab-item {
+		flex: 1;
+		padding: 12px 0;
+		cursor: pointer;
+		text-align: center;
+		background-color: #f7f7f7;
+		transition: background-color 0.3s, color 0.3s;
+		font-size: 16px;
+		color: #666666;
+	}
+
+	.tab-item.active {
+		background-color: #ffffff;
+		color: #4a90e2;
+		font-weight: bold;
+		border-top: 3px solid #4a90e2;
+	}
+
+
+
+	.tab-content {
+		padding: 20px;
+		background-color: #ffffff;
+	}
+
+	.event-item {
 		display: flex;
 		align-items: center;
-		margin-bottom: 16px;
+		margin-bottom: 10px;
 		padding: 10px;
-	}
-
-	.avatar-container {
-		padding-right: 16px;
+		background-color: #f2f2f2;
+		border-radius: 8px;
 	}
 
 	.avatar {
-		width: 40px;
-		height: 40px;
+		width: 50px;
+		height: 50px;
 		border-radius: 50%;
+		margin-right: 10px;
 	}
 
-	.notification-content {
-		flex-grow: 1;
+	.event-info {
+		display: flex;
+		flex-direction: column;
 	}
 
-	.notification-message {
+	.event-name {
+		font-size: 16px;
+		color: #333;
 		font-weight: bold;
-		margin-bottom: 4px;
 	}
 
-	.notification-time {
-		color: #888;
-		font-size: 0.8em;
+	.event-time {
+		font-size: 14px;
+		color: #666;
 	}
 </style>

fe460ab4997866a121eeb5acbf0496075aa5c522
pty <1773679235@qq.com>
2023-12-25 23:26:23 +0800

    更改统计页面

--- pages/tabbar/tabbar-2/tabbar-2.vue
+++ pages/tabbar/tabbar-2/tabbar-2.vue
@@ -1,232 +1,167 @@
 <template>
-	<view class="app">
-		<!-- 统计区域 -->
-		<view class="stats">
-			<!-- 动态显示当前选中的 Tab 统计信息 -->
-			<view class="stat-item">
-				<view class="stat-key">本周总次数</view>
-				<view class="stat-value">{{ currentStats.weekly }}</view>
-			</view>
-			<view class="stat-item">
-				<view class="stat-key">本月总次数</view>
-				<view class="stat-value">{{ currentStats.monthly }}</view>
-			</view>
-		</view>
+  <view class="container">
+    <view class="selectact">选择活动</view>
+    <picker mode="selector" :range="activities" @change="onActivityChange">
+      <view class="picker">
+        当前选择：{{ selectedActivity }}
+      </view>
+    </picker>
+    <!-- 分界 -->
+    <view class="divider"></view>
 
-		<!-- Tab 切换区域 -->
-		<view class="tabs">
-			<view class="tab-item" v-for="(tab, index) in tabs" :key="index"
-				:class="{ active: currentTab === tabs[index].value }" @click="switchTab(index)">
-				{{ tab.label }}
-			</view>
-		</view>
+    <view class="notifications-container">
+      <view class="notification" v-for="item in notifications" :key="item.id">
+        <view class="avatar-container">
+          <img :src="item.avatar" class="avatar" alt="avatar" />
+        </view>
+        <view class="notification-content">
+          <view class="notification-message">{{ item.name }} - {{ getActivityCountForPerson(item) }}次</view>
+          <view class="notification-time">{{ item.time }}</view>
+        </view>
+      </view>
+    </view>
 
-		<!-- 当前 Tab 内容区域 -->
-		<view class="tab-content">
-			<view class="event-item" v-for="event in currentTabEvents" :key="event.id" @click="goToReplay(item)">
-				<image :src="event.avatar" class="avatar" />
-				<view class="event-info">
-					<view class="event-name">{{ event.name }}</view>
-					<view class="event-time">{{ event.time }}</view>
-				</view>
-			</view>
-		</view>
-	</view>
+    <view class="info">
+      <text v-if="selectedActivity">「{{ selectedActivity }}」的统计总次数是：{{ selectedCount }}</text>
+      <text v-else>请选择一个活动</text>
+    </view>
+  </view>
 </template>
 
 <script setup>
-	import {
-		ref,
-		reactive,
-		computed
-	} from 'vue';
-	const goToReplay = (item) => {
-		const url = `/pages/tabbar/tabbar-1/video_replay?item=${item}`
-		console.log(url);
-		uni.navigateTo({
-			url: url
-		})
-	};
-	const tabEvents = {
-		tab1: [{
-				id: 1,
-				name: '张三',
-				time: '2023-12-02 10:30',
-				avatar: '../../../static/img/face/face2.png'
-			},
-			{
-				id: 2,
-				name: '李四',
-				time: '2023-12-17 16:30',
-				avatar: '../../../static/img/face/face3.png'
-			},
-		],
-		tab2: [{
-				id: 1,
-				name: '王五',
-				time: '2023-12-02 10:30',
-				avatar: '../../../static/img/face/face4.png'
-			},
-			{
-				id: 2,
-				name: '赵六',
-				time: '2023-12-17 16:30',
-				avatar: '../../../static/img/face/face5.png'
-			},
-		],
-		tab3: [{
-				id: 1,
-				name: '孙七',
-				time: '2023-12-02 10:30',
-				avatar: '../../../static/img/face/face6.png'
-			},
-			{
-				id: 2,
-				name: '王八',
-				time: '2023-12-17 16:30',
-				avatar: '../../../static/img/face/face7.png'
-			},
-		]
-	};
+import { ref, computed } from 'vue';
 
-	const tabStats = {
-		tab1: {
-			weekly: 10,
-			monthly: 40
-		},
-		tab2: {
-			weekly: 5,
-			monthly: 20
-		},
-		tab3: {
-			weekly: 20,
-			monthly: 80
-		},
-	};
+const activities = ['测体温', '翻身', '换尿不湿', '换衣服', '喂水', '喂饭', '喂药', '排尿排便', '洗脚', '剪指甲']; // 假设的活动列表
+const selectedActivity = ref('');
 
-	const tabs = reactive([{
-			label: '翻身',
-			value: 'tab1'
-		},
-		{
-			label: '喝水',
-			value: 'tab2'
-		},
-		{
-			label: '穿衣',
-			value: 'tab3'
-		},
-	]);
-	const currentTabEvents = computed(() => {
-		return tabEvents[currentTab.value] || [];
-	});
-	const currentTab = ref(tabs[0].value);
+const notifications = ref([
+  {
+    id: 1,
+    name: '张三',
+    time: '2023-12-02 10:30',
+    avatar: '../../../static/img/face/face2.png',
+    activities: {
+      '测体温': 2, '翻身': 5, '换尿不湿': 3, '换衣服': 1, '喂水': 2, '喂饭': 4, '喂药': 1, '排尿排便': 3, '洗脚': 2, '剪指甲': 3
+    }
+  },
+  {
+    id: 2,
+    name: '李四',
+    time: '2023-12-02 10:30',
+    avatar: '../../../static/img/face/face3.png',
+    activities: {
+      '测体温': 1, '翻身': 2, '换尿不湿': 4, '换衣服': 2, '喂水': 3, '喂饭': 5, '喂药': 2, '排尿排便': 1, '洗脚': 4, '剪指甲': 2
+    }
+  },
+  {
+    id: 3,
+    name: '王五',
+    time: '2023-12-02 10:30',
+    avatar: '../../../static/img/face/face4.png',
+    activities: {
+      '测体温': 3, '翻身': 1, '换尿不湿': 1, '换衣服': 3, '喂水': 1, '喂饭': 2, '喂药': 3, '排尿排便': 2, '洗脚': 1, '剪指甲': 4
+    }
+  }
+]);
 
-	const currentStats = computed(() => {
-		return tabStats[currentTab.value];
-	});
 
-	function switchTab(index) {
-		currentTab.value = tabs[index].value;
-	}
+const selectedCount = computed(() => {
+  if (!selectedActivity.value) return 0;
+  return notifications.value.reduce((sum, item) => sum + (item.activities[selectedActivity.value] || 0), 0);
+});
+
+const getActivityCountForPerson = (person) => {
+  return person.activities[selectedActivity.value] || 0;
+};
+
+const onActivityChange = (event) => {
+  const activity = activities[event.detail.value];
+  selectedActivity.value = activity;
+};
 </script>
 
-<style scoped>
-	.app {
-		font-family: 'Arial', sans-serif;
-		max-width: 600px;
-		margin: auto;
-		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-		border-radius: 8px;
-		overflow: hidden;
-		height: 100%;
-	}
 
-	.stats {
-		display: flex;
-		justify-content: space-around;
-		background-color: #ffffff;
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.selectact{
+	font-weight: bold;
+	margin-bottom: 15px;
+}
+
+.picker{
+	margin-right: auto;
+	margin-left: auto;
+	
+}
+
+.picker {
+  margin-bottom: 5px;
+  color: #FFF;
+  background-color:  #FFBB98;
+  border: 1px solid #ddd;
+  border-radius: 15px;
+  padding: 10px;
+}
+
+.info {
+  margin-top: 20px;
+}
+
+.divider {
+	height: 1px;
+	background-color: #e0e0e0;
+	width: 100%;
+	margin: 20px 0;
+}
+
+
+.notifications-container {
 		padding: 16px;
-		border-bottom: 1px solid #eee;
+		height: 100%;
+		overflow-y: auto;
+		width: 100%;
 	}
 
-	.stat-item {
-		text-align: center;
-	}
-
-	.stat-key {
-		font-size: 14px;
-		color: red;
-		margin-bottom: 8px;
-	}
-
-	.stat-value {
-		font-size: 20px;
-		color: #333333;
-		font-weight: bold;
-	}
-
-	.tabs {
-		display: flex;
-		background-color: #f7f7f7;
-		overflow: auto;
-		/* 允许水平滚动 */
-	}
-
-	.tab-item {
-		flex: 1;
-		padding: 12px 0;
-		cursor: pointer;
-		text-align: center;
-		background-color: #f7f7f7;
-		transition: background-color 0.3s, color 0.3s;
-		font-size: 16px;
-		color: #666666;
-	}
-
-	.tab-item.active {
-		background-color: #ffffff;
-		color: #4a90e2;
-		font-weight: bold;
-		border-top: 3px solid #4a90e2;
-	}
-
-
-
-	.tab-content {
-		padding: 20px;
-		background-color: #ffffff;
-	}
-
-	.event-item {
+	.notification {
+		background-color: #d9d9d9;
+		border-radius: 10px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 		display: flex;
 		align-items: center;
-		margin-bottom: 10px;
+		margin-bottom: 16px;
 		padding: 10px;
-		background-color: #f2f2f2;
-		border-radius: 8px;
+	}
+
+	.avatar-container {
+		padding-right: 16px;
 	}
 
 	.avatar {
-		width: 50px;
-		height: 50px;
+		width: 40px;
+		height: 40px;
 		border-radius: 50%;
-		margin-right: 10px;
 	}
 
-	.event-info {
-		display: flex;
-		flex-direction: column;
+	.notification-content {
+		flex-grow: 1;
 	}
 
-	.event-name {
-		font-size: 16px;
-		color: #333;
+	.notification-message {
 		font-weight: bold;
+		margin-bottom: 4px;
 	}
 
-	.event-time {
-		font-size: 14px;
-		color: #666;
+	.notification-time {
+		color: #888;
+		font-size: 0.8em;
 	}
+
+
 </style>

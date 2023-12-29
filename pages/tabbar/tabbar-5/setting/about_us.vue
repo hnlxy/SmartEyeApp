6d5dee230f50ebfc77ded827fcbc4b13f0d64658
pty <1773679235@qq.com>
2023-12-29 13:31:11 +0800

    增加修改pin和关于我们页面

--- pages/tabbar/tabbar-5/setting/about_us.vue
+++ pages/tabbar/tabbar-5/setting/about_us.vue
@@ -1,8 +1,86 @@
 <template>
+  <view class="page-container">
+    <view class="logo-container">
+      <image class="logo" src="/static/logo.png" />
+    </view>
+    <view class="list-container">
+      <view class="list-item" @click="goToVersion">
+        <text>当前版本</text>
+      </view>
+      <view class="list-item" @click="goToUpdate">
+        <text>版本更新</text>
+      </view>
+      <view class="list-item" @click="goToFeatures">
+        <text>功能介绍</text>
+      </view>
+    </view>
+    <view class="footer">
+      <text class="footer-text" @click="goToTerms">服务协议</text>
+      <text class="footer-text" @click="goToPrivacy">隐私政策</text>
+    </view>
+  </view>
 </template>
 
-<script>
+<script setup>
+// 在这里添加您的 Vue 3 逻辑
+
+function goToVersion() {
+  // 导航到当前版本页面
+}
+
+function goToUpdate() {
+  // 导航到版本更新页面
+}
+
+function goToFeatures() {
+  // 导航到功能介绍页面
+}
+
+function goToTerms() {
+  // 导航到服务协议页面
+}
+
+function goToPrivacy() {
+  // 导航到隐私政策页面
+}
 </script>
 
-<style>
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.logo-container {
+	margin-top: 30px;
+	margin-bottom: 30px;
+}
+
+.logo {
+  width: 100px;
+  height: 100px; 
+}
+
+.list-container {
+  width: 100%;
+  margin-top: 30px;
+}
+
+.list-item {
+  padding: 15px;
+  border-bottom: 1px solid #eee;
+  text-align: left;
+}
+
+.footer {
+  margin-top: 30px;
+  font-size: 12px;
+}
+
+.footer-text {
+  margin-right: 10px;
+  color: #007aff;
+}
 </style>
--- pages/tabbar/tabbar-5/setting/change_PIN.vue
+++ pages/tabbar/tabbar-5/setting/change_PIN.vue
@@ -1,9 +1,103 @@
 <template>
-	<view>更改pin</view>
+	<view class="container">
+		<view class="input-container">
+			<input v-model="phoneNumber" type="text" placeholder="请输入您的手机号" />
+		</view>
+		<view class="input-container">
+			<input v-model="verificationCode" type="text" placeholder="请输入验证码" />
+			<button @click="sendVerificationCode" :disabled="isCountingDown">
+				{{ isCountingDown ? `${countdown}秒后重试` : '发送验证码' }}
+			</button>
+		</view>
+		<view class="input-container">
+			<input v-model="uid" type="text" placeholder="请输入新密码" />
+		</view>
+		<button class="bind-button" @click="bindUid">修改密码</button>
+	</view>
 </template>
 
-<script>
+<script setup>
+	import {
+		ref,
+		watch
+	} from 'vue';
+
+	const phoneNumber = ref('');
+	const verificationCode = ref('');
+	const uid = ref('');
+	const countdown = ref(60);
+	const isCountingDown = ref(false);
+
+	const sendVerificationCode = () => {
+		console.log(`发送验证码到手机号: ${phoneNumber.value}`);
+		// 在这里添加发送验证码的逻辑
+		startCountdown();
+	};
+
+	const startCountdown = () => {
+		if (isCountingDown.value) {
+			return;
+		}
+
+		isCountingDown.value = true;
+		countdown.value = 60;
+		const interval = setInterval(() => {
+			countdown.value--;
+			if (countdown.value === 0) {
+				clearInterval(interval);
+				isCountingDown.value = false;
+			}
+		}, 1000);
+	};
+
+	const bindUid = () => {
+		console.log(`绑定的手机号是: ${phoneNumber.value}, 验证码是: ${verificationCode.value}, UID是: ${uid.value}`);
+		// 在这里添加绑定UID的逻辑
+	};
 </script>
 
 <style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	.input-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 20px;
+		width: 100%;
+	}
+
+	.input-container input {
+		flex: 1;
+		margin-right: 10px;
+		margin-left: 10px;
+	}
+
+	.input-container button {
+		flex-shrink: 0;
+	}
+
+	.bind-button {
+		background-color: #4CAF50;
+		color: white;
+		padding: 10px 20px;
+		border: none;
+		border-radius: 4px;
+		cursor: pointer;
+		width: 50%;
+		line-height: 20px;
+	}
+
+	input {
+		border: 1px solid #ccc;
+		padding: 10px;
+		border-radius: 4px;
+		width: 70%;
+	}
 </style>
